0c0b085f362adbf323e84c60cf540af7b24e2bb3
Terry Robb <terranceprobb@gmail.com>
2022-10-25 11:48:48 -0400

    2022 update

--- js/tr.js
+++ js/tr.js
@@ -47,40 +47,40 @@
     	dirty.scroll = false;
     }());
 
-    function homeAnimation(){
-      var logoHover = anime.timeline({
-        autoplay: false,
-        easing: 'easeOutBack'
-      });
-      logoHover
-      .add({
-        targets: '.custom-logo-link p',
-        scaleX: 0.85,
-        duration: 1
-      },0)
-      .add({
-        targets: '.custom-logo-link svg',
-        duration: 150,
-        opacity: 0,
-        scaleY: .85
-      })
-      .add({
-        targets: '.custom-logo-link p',
-        opacity: 1,
-        scaleX: 1,
-        duration: 150
-      },'-=125');
-      $('.custom-logo-link').hover(
-        function(){
-          if (logoHover.reversed) logoHover.reverse();
-          logoHover.play();
-        },
-        function(){
-          if (!logoHover.reversed) logoHover.reverse();
-          logoHover.play();
-        }
-      );
-    }
+    // function homeAnimation(){
+    //   var logoHover = anime.timeline({
+    //     autoplay: false,
+    //     easing: 'easeOutBack'
+    //   });
+    //   logoHover
+    //   .add({
+    //     targets: '.custom-logo-link p',
+    //     scaleX: 0.85,
+    //     duration: 1
+    //   },0)
+    //   .add({
+    //     targets: '.custom-logo-link svg',
+    //     duration: 150,
+    //     opacity: 0,
+    //     scaleY: .85
+    //   })
+    //   .add({
+    //     targets: '.custom-logo-link p',
+    //     opacity: 1,
+    //     scaleX: 1,
+    //     duration: 150
+    //   },'-=125');
+    //   $('.custom-logo-link').hover(
+    //     function(){
+    //       if (logoHover.reversed) logoHover.reverse();
+    //       logoHover.play();
+    //     },
+    //     function(){
+    //       if (!logoHover.reversed) logoHover.reverse();
+    //       logoHover.play();
+    //     }
+    //   );
+    // }
 
     function parallaxMain() {
         var scrolltop = window.pageYOffset
@@ -95,7 +95,7 @@
             scroll.style.top = scrollValue + 'rem'
             scroll2.style.top = scrollValue*1.5 + 'rem'
           }else {
-            scroll2.style.opacity = 1 - scrollValue*-0.15
+            scroll2.style.opacity = 1 - scrollValue*-0.1
           }
     	}
     }
@@ -105,13 +105,13 @@
     if ($("body").hasClass("home")){
       particlesJS("particles-js", {
         particles: {
-            number: { value: 21, density: { enable: true, value_area: 800 } },
-            color: { value: "#fa726e" },
-            shape: { type: "polygon", polygon: { nb_sides: 4 } },
-            opacity: { value: 1, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
-            size: { value: 4, random: true, anim: { enable: false, speed: 3, size_min: 40, sync: false } },
+            number: { value: 100, density: { enable: true, value_area: 500 } },
+            color: { value: "#fff" },
+            shape: { type: "polygon", polygon: { nb_sides: 12 } },
+            opacity: { value: 0.4, random: true, anim: { enable: true, speed: 0.2, opacity_min: 0.1, sync: false } },
+            size: { value: 2, random: true, anim: { enable: false, speed: 3, size_min: 40, sync: false } },
             line_linked: { enable: false, distance: 200, color: "#ffffff", opacity: 1, width: 2 },
-            move: { enable: true, speed: 0.25, direction: "bottom-right", random: false, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } },
+            move: { enable: true, speed: 0.12, direction: "bottom-right", random: true, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } },
         },
 				"interactivity": {
 			    "detect_on": "canvas",
@@ -130,13 +130,13 @@
   } else {
     particlesJS("particles-js", {
       particles: {
-          number: { value: 42, density: { enable: true, value_area: 800 } },
-          color: { value: "#ffffff" },
-          shape: { type: "polygon", polygon: { nb_sides: 4 } },
-          opacity: { value: 1, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
-          size: { value: 4, random: true, anim: { enable: false, speed: 3, size_min: 40, sync: false } },
-          line_linked: { enable: false, distance: 200, color: "#ffffff", opacity: 1, width: 2 },
-          move: { enable: true, speed: 0.25, direction: "none", random: false, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } },
+				number: { value: 125, density: { enable: true, value_area: 250 } },
+				color: { value: "#fff" },
+				shape: { type: "polygon", polygon: { nb_sides: 12 } },
+				opacity: { value: 0.3, random: true, anim: { enable: true, speed: 0.5, opacity_min: 0.1, sync: false } },
+				size: { value: 2, random: true, anim: { enable: false, speed: 3, size_min: 40, sync: false } },
+				line_linked: { enable: false, distance: 200, color: "#ffffff", opacity: 1, width: 2 },
+				move: { enable: true, speed: 1.2, direction: "top", random: true, straight: false, out_mode: "out", bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } },
       },
 			"interactivity": {
 				"detect_on": "canvas",
@@ -156,9 +156,9 @@
 
     });
   	$win.load(function() {
-      if($win.width() > 960){
-        homeAnimation();
-      }
+      // if($win.width() > 960){
+      //   homeAnimation();
+      // }
 
     });
     $win.scroll(function() {dirty.scroll = true;});
